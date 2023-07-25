d328b281e4ce90d9caa97c9c4243816dd9d99138
Biruk <104642908+Barok-Getachew@users.noreply.github.com>
2023-07-25 13:11:30 +0300

    Time: 0 ms (100.00%) | Memory: 41.3 MB (9.22%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        
+            int length = digits.length;
+                for(int i = length - 1 ; i>=0;i--){
+                    if(digits[i] < 9 ){
+                        digits[i]++;
+                        return digits;
+                    }
+                    digits[i] = 0;
+                }
+                int newDigit[] = new int[length + 1];
+                newDigit[0] = 1;
+
+
+                return newDigit;
+    }
+}
