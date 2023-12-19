6d29ebb8218ec8d74e2f7ad8013d2c67c1cd6218
Biruk Getachew <104642908+Barok-Getachew@users.noreply.github.com>
2023-12-19 17:39:51 +0300

    Time: 1 ms (78.85%) | Memory: 40.2 MB (96.33%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+    
+        StringBuilder result = new StringBuilder(); 
+  
+
+        for (int i = 0; i < word1.length() || i < word2.length(); i++) { 
+  
+        
+            if (i < word1.length()) 
+                result.append(word1.charAt(i)); 
+  
+            if (i < word2.length()) 
+                result.append(word2.charAt(i)); 
+        } 
+  
+        return result.toString(); 
+    }
+}
