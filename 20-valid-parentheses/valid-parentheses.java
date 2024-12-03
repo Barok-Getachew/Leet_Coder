ae8deeac3ec8eee7b5d1eda5ed55f13b8b882fbb
Biruk Getachew <104642908+Barok-Getachew@users.noreply.github.com>
2024-12-03 14:15:25 +0300

    Time: 3 ms (42.78%) | Memory: 42.1 MB (12.52%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+   
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);  
+            } else {
+             
+                if (stack.isEmpty() || 
+                   (c == ')' && stack.peek() != '(') ||
+                   (c == ']' && stack.peek() != '[') ||
+                   (c == '}' && stack.peek() != '{')) {
+                    return false;
+                }
+                stack.pop();  
+            }
+        }
+       
+        return stack.isEmpty();
+    }
+}
